8cda86d2026191b76fcf19e992647ac20b9f2257
Revanth Sai Sreerangam <revanthsai.work@gmail.com>
2025-09-01 14:36:32 +0530

    Add files via upload

---
+++ EXP12-d.cpp
@@ -0,0 +1,51 @@
+//Harsh kumar
+//24070123146
+//B3
+//Copy Constructor
+
+#include<iostream>
+#include<string.h>
+using namespace std;
+class student
+{
+    int rno;
+    char name[50];
+    double fee;
+    public:
+    student(int,char[],double);
+student(student &t)     
+    {
+        rno=t.rno;
+        strcpy(name,t.name);
+        fee=t.fee;
+    }
+    void display();
+
+};
+student::student(int no,char n[],double f)
+    {
+        rno=no;
+        strcpy(name,n);
+        fee=f;
+    }    
+
+   void student::display()
+    {
+        cout<<endl<<rno<<"\t"<<name<<"\t"<<fee;
+    }
+
+int main()
+{
+    student s(146,"Harsh",300000);
+s.display();
+
+    student manjeet(s);  
+manjeet.display();
+
+    return 0;
+}
+
+/* output:
+146	Harsh	300000
+146	Harsh	300000
+*/
